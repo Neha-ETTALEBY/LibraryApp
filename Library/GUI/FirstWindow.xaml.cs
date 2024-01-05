2574e1c2d157281191073e5581f00c5a883430fd
Nehaett <142404634+Neha-ETTALEBY@users.noreply.github.com>
2024-01-05 18:11:48 +0100

    Choice Role page created

---
+++ Library/GUI/FirstWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library.GUI
+{
+    /// <summary>
+    /// Logique d'interaction pour FirstWindow.xaml
+    /// </summary>
+    public partial class FirstWindow : Window
+    {
+        public FirstWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AdherentRoleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            Accueil home = new Accueil(); //à partir du page  d'acceuil il peut soit se connecter soit s'inscrire
+                home.Show();
+                Hide();
+            
+        }
+
+        private void AdminRolebtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmployerRoleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- Library/MainWindow.xaml.cs
+++ Library/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace Library
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
